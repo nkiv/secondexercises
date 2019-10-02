7c100b5b443fd8fbb74ebe497c6c84a0e3ba9fbf
Noah Tyler Kivett <kivettnt@s.dcsdk12.org>
2019-10-02 13:12:01 -0600

    .

---
+++ Chapter25/TriangleSquare.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TriangleSquare{
+
+    public static void main(String [] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insert max number");
+
+        int pattern = 1;
+        int max = scan.nextInt();
+        int add = 2;
+        
+        int squarePattern = 1;
+        int squareAdd = 3; 
+
+        while(pattern <= max || squarePattern <= max){
+            System.out.println(pattern + " " + squarePattern);
+            pattern = pattern + add;
+            squarePattern = squarePattern + squareAdd;
+            add++;
+            squareAdd = squareAdd + 2;
+        }
+    }
+
+}
