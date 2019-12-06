810e3cd2982382a08b8521295dfaf1a6829d3671
Noah Tyler Kivett <kivettnt@s.dcsdk12.org>
2019-12-06 12:51:52 -0700

    FIngished

---
+++ Chapter54/Odious.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Odious{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        boolean process = true;
+        System.out.println("Type a number stupid weenie");
+        int userNumb = scan.nextInt();
+        int saveuserNumb = userNumb;
+        int odiousCount = 0;
+
+        while(process){
+            int binary = userNumb%2;
+
+            if(binary == 1){
+                odiousCount++;
+            }
+            int dividend = userNumb/2;
+
+            if(dividend == 0){
+                process = false;
+            }
+            userNumb = dividend;
+        }
+        System.out.println("--------------------------");
+        if ((odiousCount % 2) == 1){
+            System.out.println(saveuserNumb + " is an Odious Number");
+        } else{
+            System.out.println(saveuserNumb + " is an evil Number!");
+        }
+        System.out.println("--------------------------");
+    }
+}
