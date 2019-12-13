a1a5b5f8b46c79e7be847514afd281c7db816bf5
Noah Tyler Kivett <kivettnt@s.dcsdk12.org>
2019-12-13 12:52:07 -0700

    Start binald

---
+++ Chapter60/ThreeArrays.java
@@ -0,0 +1,18 @@
+public class ThreeArrays{
+
+  public static void main ( String[] args ){
+      
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {-12,  24, -79, -13};
+    int[] sum    = {  0,   0,   0,   0};
+    
+    // Add values from corresponding cells of valA and valB
+    // and put the result in the corresponding cell of sum.
+
+
+
+ 
+    System.out.println( "sum: " 
+        + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+   }
+}
